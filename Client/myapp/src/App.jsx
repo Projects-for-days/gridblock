0a46e012e18111b67280fb4bc1e47cb95dc9dfb1
Ajiikn <godswillajii14@gmail.com>
2026-02-21 22:47:57 +0100

    fixed timer

--- Client/myapp/src/App.jsx
+++ Client/myapp/src/App.jsx
@@ -1,10 +1,12 @@
 import Board from "./Components/Board.jsx";
+import Timer from "./Components/Timer.jsx";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Grid Block</h1>
+      <Timer />
       <Board />
     </div>
   );
--- Client/myapp/src/Components/Timer.jsx
+++ Client/myapp/src/Components/Timer.jsx
@@ -1,7 +1,29 @@
-import { startTurn } from '../Utils/timer';
+import { useState, useRef } from "react";
 
 export default function Timer() {
-    return (
-        <div><button onClick={() => startTurn(15)}>Start Timer</button></div>
-    );
+  const [time, setTime] = useState(0);
+  const intervalRef = useRef(null);
+
+  function startTimer(seconds) {
+    clearInterval(intervalRef.current);
+
+    setTime(seconds);
+
+    intervalRef.current = setInterval(() => {
+      setTime((prev) => {
+        if (prev == 0) {
+          clearInterval(intervalRef.current);
+          return 0;
+        }
+        return prev - 1;
+      });
+    }, 1000);
+  }
+
+  return (
+    <div>
+      <h1>{time}</h1>
+      <button onClick={() => startTimer(15)}>Start Game</button>
+    </div>
+  );
 }
